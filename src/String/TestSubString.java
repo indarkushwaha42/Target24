b8921b83f10c37637edb98527f9d0218f5d9dd6e
indar <indarkushwaha42@gmail.com>
2024-01-04 15:50:08 +0530

    created substring of String

---
+++ src/String/TestSubString.java
@@ -0,0 +1,9 @@
+package String;
+
+public class TestSubString {
+    public static void main(String[] args) {
+        String s = "this is digital technology era";
+        String ss = s.substring(0);
+        System.out.println(ss);
+    }
+}
